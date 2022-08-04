cdf0aa1baf2100340063d2780c1844b66c5044a1
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-04 14:11:00 -0400

    Revert "Created initial database using migration"
    This reverts commit e23554b3d0b81aed0196715e5688b006d1765beb.

--- JobTracker/Startup.cs
+++ JobTracker/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using JobTracker.Data;
 
 namespace JobTracker
 {
@@ -26,9 +24,6 @@ namespace JobTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-
-            services.AddDbContext<JobTrackerContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("JobTrackerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
