13460d92aa0a89a33da23e61d1d29fccf2220faf
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-04 01:54:29 -0400

    Created Data Model for Job and DTR
    Created Data Models for Job and DTR

---
+++ JobTracker/Models/DTR.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobTracker.Models
+{
+    public class DTR
+    {
+            [Key]
+            public int DTRId { get; set; }
+
+            [DataType(DataType.DateTime)]
+            public string TimeIn { get; set; }
+            [DataType(DataType.DateTime)]
+            public string TimeOut { get; set; }
+
+            [Display(Name = "Job")]
+            public int JobId { get; set; }
+
+            [ForeignKey("JobId")]
+            public virtual Job Jobs { get; set; }
+    }
+}
+
---
+++ JobTracker/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+        [Required]
+        public string Position { get; set; }
+        [Required]
+        public string Company { get; set; }
+        public string Detail { get; set; }
+        public string Status { get; set; }
+    }
+}
+
