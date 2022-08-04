9cd49935fb02bf3cc27fac929d9337ba4927cb74
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-04 02:01:04 -0400

    Created basic UI for Jobs using scaffolding
    Created basic UI for Jobs using scaffolding

---
+++ JobTracker/Pages/Jobs/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using JobTracker.Data;
+using JobTracker.Models;
+
+namespace JobTracker.Pages.Jobs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly JobTracker.Data.JobTrackerContext _context;
+
+        public CreateModel(JobTracker.Data.JobTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Job Job { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Job.Add(Job);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
